fb487ffcc00bdc2c05d4f5a668a527bc6be7e469
DK <darshan@bullkingtimes.com>
2024-12-18 21:32:51 +0530

    added seo

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -4,9 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataService {
-  title:string =  "FlyProductionHouse - Your Premier Photography and Video Editing Services";
-  description:string = "Experience top-notch photography and video editing services from FlyProductionHouse. We specialize in capturing stunning visuals and creating captivating videos."
-  keywords:string = "photography, video editing, FlyProductionHouse, professional photography, video production, digital photography, video post-production, cinematic videography, photography studio, video editing studio, wedding photography services, corporate video production, commercial photography, product photography, real estate photography, event videography, music video production, aerial photography, drone videography, 4K video production"
-  viewport:string = "width=device-width, initial-scale=1"
+  title:string =  "Fly Production House - One of the best photography and video editing services in mumbai";
+  description:string = "Experience top-notch photography and video editing services from Fly Production House. We specialize in capturing stunning visuals and creating captivating videos."
+  keywords:string = "Fly Production House offers photography, video editing, professional photography, video production, digital photography, video post-production, cinematic videography, photography studio, video editing studio, wedding photography services, corporate video production, commercial photography, product photography, real estate photography, event videography, music video production, aerial photography, drone videography, 4K video production"
+  viewport:string = "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
   constructor() { }
 }
