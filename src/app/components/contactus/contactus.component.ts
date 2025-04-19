71cee4ccf76670b7575a5abb581c6f20919e9212
divyesh <divyesh@divyeshs-MacBook-Pro.local>
2025-04-19 13:43:29 +0530

    email added

--- src/app/components/contactus/contactus.component.ts
+++ src/app/components/contactus/contactus.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Meta, Title } from '@angular/platform-browser';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contactus',
@@ -16,6 +17,26 @@ export class ContactusComponent {
   address:string = 'Vasari Hill Rd, Rustomjee Ease Zone Mall Malad West, Mumbai, Maharashtra 400104. 1st Floor Office no - 3068'
 
   constructor(private dataService: DataService, private title: Title, private meta: Meta) {}
+
+  sendEmail(event: Event) {
+    event.preventDefault(); // Prevent page reload
+
+    emailjs.sendForm(
+      'service_ntkgkpj',
+      'template_2rgie1a',
+      event.target as HTMLFormElement,
+      '34Of9h2idRUBQAfCE'
+    )
+    .then((result) => {
+      console.log('Email successfully sent!', result.text);
+      alert('Message sent successfully!');
+      (event.target as HTMLFormElement).reset();
+    })
+    .catch((error) => {
+      console.error('Error sending email:', error.text);
+      alert('Oops! Something went wrong.');
+    });
+  }
 
   ngOnInit(): void {
     this.title.setTitle(this.dataService.title);
